a8f42845a16a7ff22235ee7531af7406968fe8c1
lemba <lemba.nsimba.arnaud.freelance@gmail.com>
2024-03-11 23:33:28 +0100

    last version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
+
 import Header from './component/Header.js';
-
 import './css/App.css';
-
-
 import videoBg from './assets/foret.mp4';
 
 
@@ -10,36 +8,13 @@ function App() {
 
 
   return (
-
-
-
-
-      <>
-
-
-        <video src={videoBg} autoPlay loop muted />
-
-          <div className="content"> 
-    
-              <Header/>
-
-          </div>
-
-       
-
-       </>
-   
-    
-
-
-    
-
-   
-
-
+    <>
+      <video src={videoBg} autoPlay loop muted />
+      <div className="content"> 
+        <Header/>
+      </div>
+    </>
   );
 }
 
 export default App;
-
-//      <img src={logo} className="App-logo" alt="logo" />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+import { MongoClient } from 'mongodb';
+
+const uri = "mongodb+srv://lemba:cyril@petits.bzoed29.mongodb.net/net_class?retryWrites=true&w=majority&appName=petits";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
+  console.log('Connected to MongoDB');
+});
+
+*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
