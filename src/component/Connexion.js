e38e031ccfea5713e32273af3c9cdfe76086bad4
lemba <lemba.nsimba.arnaud.freelance@gmail.com>
2024-03-18 15:30:55 +0100

    save user by pseudo and password scripted

--- src/component/Connexion.js
+++ src/component/Connexion.js
@@ -41,25 +41,29 @@ function Connexion() {
     };
 
     // Fonction de hachage sécurisé du mot de passe
-    const encryptPassword = async (password) => {
-        try {
-            const saltRounds = 10; // Nombre de tours de salage pour bcrypt
-            const hashedPassword = await bcrypt.hash(password, saltRounds);
-            return hashedPassword;
-        } catch (error) {
-            console.error("Erreur lors du hachage du mot de passe:", error);
-            throw new Error("Erreur lors du hachage du mot de passe");
-        }
-    };
+const encryptPassword = async (password) => {
+    try {
+        const saltRounds = 10; // Nombre de tours de salage pour bcrypt
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        console.log("Hachage du mot de passe généré avec succès :", hashedPassword); // Ajout d'un journal pour afficher le hachage généré
+        return hashedPassword;
+    } catch (error) {
+        console.error("Erreur lors du hachage du mot de passe:", error);
+        throw new Error("Erreur lors du hachage du mot de passe");
+    }
+};
 
     return (
         <div className="formulaire">
             <p> Veuillez vous connecter ! </p>
             <form onSubmit={handleSubmit}>
                 <div>
-                    <label> Pseudo : <input type="text" name="pseudo" value={formData.pseudo} onChange={handleChange} /> </label> <br />
+
+                    <pre> 
+                    <label> Pseudo :       <input type="text" name="pseudo" value={formData.pseudo} onChange={handleChange} /> </label> <br />
                     <label> Mot de passe : <input type="password" name="password" value={formData.password} onChange={handleChange} /> </label> <br />
                     <input type="submit" value="Connexion/Inscription" id="aligner-button" />
+                    </pre>
                 </div>
             </form>
             {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
