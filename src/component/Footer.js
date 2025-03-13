4e548a0e078b64eb49bee42120f8abb51d771862
Nsimba01 <lemba.nsimba.arnaud.freelance@gmail.com>
2025-03-13 15:35:12 +0100

    footer in building ...

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,22 +7,68 @@ import '../css/footer.css';
 function Footer() {
   return (
 
+    <> 
 
     <div className="footer">
 
 
-        <div>
+         <div className='footer-position'>
 
-            Cyril
+              <h3> Présentation </h3>
+
+              <p> Qui sommes nous ?  </p>
+
+              <p> Notre mission  </p>
 
          </div>
-         <div>
 
-        Contact :  cyril.nguyen@live.fr
+         <div className='footer-position'>
+
+    
+              <h3> Informations légales </h3>
+
+              <p> Conditions générales </p>
+
+              <p> Politique de confidencialité </p>
+
+              <p> Gestion de cookies </p>
+
+         </div>
+
+
+         
+         <div className='footer-position'>
+
+    
+              <h3>Partenaires </h3>
+
+              <p> Nos partenaires </p>
+
+              <p> Dévenir partenaires </p>
+
+              <p> Nous aider</p>
+
+         </div>
+
+
+           
+         <div className='footer-position'>
+
+    
+              <h3> Aide et contact </h3>
+
+              <p> Prise en main </p>
+
+              <p> Contact</p>
+
+       
 
          </div>
 
      </div>
+
+
+     </>
                   
   );
 }
