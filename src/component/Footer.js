8dec1ef759318928c5756fa0d72e671b0d4e9e3a
Nsimba01 <lemba.nsimba.arnaud.freelance@gmail.com>
2025-03-14 11:03:36 +0100

    footer in building ...

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -9,63 +9,42 @@ function Footer() {
 
     <> 
 
-    <div className="footer">
+<div className="footer">
+    <div className="footer-content">
+        <div className='footer-position'>
+            <h3 className='title-design'> Présentation </h3>
+            <p> Qui sommes nous ?  </p>
+            <p> Notre mission  </p>
+        </div>
+
+        <div className='footer-position'>
+            <h3 className='title-design'> Informations légales </h3>
+            <p> Conditions générales </p>
+            <p> Politique de confidentialité </p>
+            <p> Gestion de cookies </p>
+        </div>
+
+        <div className='footer-position'>
+            <h3  className='title-design'> Partenaires </h3>
+            <p> Nos partenaires </p>
+            <p> Devenir partenaires </p>
+            <p> Nous aider</p>
+        </div>
+
+        <div className='footer-position'>
+            <h3 className='title-design'> Aide et contact </h3>
+            <p> Prise en main </p>
+            <p> Contact</p>
+        </div>
+    </div>
+
+    <div className="footer-legal">
+        @2024 arbredusavoir.fr (tous droits réservés)
+    </div>
+</div>
 
 
-         <div className='footer-position'>
 
-              <h3> Présentation </h3>
-
-              <p> Qui sommes nous ?  </p>
-
-              <p> Notre mission  </p>
-
-         </div>
-
-         <div className='footer-position'>
-
-    
-              <h3> Informations légales </h3>
-
-              <p> Conditions générales </p>
-
-              <p> Politique de confidencialité </p>
-
-              <p> Gestion de cookies </p>
-
-         </div>
-
-
-         
-         <div className='footer-position'>
-
-    
-              <h3>Partenaires </h3>
-
-              <p> Nos partenaires </p>
-
-              <p> Dévenir partenaires </p>
-
-              <p> Nous aider</p>
-
-         </div>
-
-
-           
-         <div className='footer-position'>
-
-    
-              <h3> Aide et contact </h3>
-
-              <p> Prise en main </p>
-
-              <p> Contact</p>
-
-       
-
-         </div>
-
-     </div>
 
 
      </>
